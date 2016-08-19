32a18c5314416750235c4f9a4d2cc98354cebcb8
Jesse Aldridge <JesseAldridge@gmail.com>
2016-08-19 16:19:01 -0700

    add debug info

--- loop.py
+++ loop.py
@@ -1,7 +1,9 @@
 import subprocess, os
 
 while True:
+    print 'running app_checker.py...'
     proc = subprocess.Popen('python app_checker.py'.split(), stdout=subprocess.PIPE)
+    print 'done'
     stdout = proc.communicate()[0]
     with open(os.path.expanduser('~/app_checker_report.txt'), 'w') as f:
         f.write(stdout)
