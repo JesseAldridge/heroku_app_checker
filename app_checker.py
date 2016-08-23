3967f23174f3609b0baa4d4ee041503121cd14ba
Jesse Aldridge <JesseAldridge@gmail.com>
2016-08-22 17:22:39 -0700

    don't specify remote

--- app_checker.py
+++ app_checker.py
@@ -10,7 +10,7 @@ import secrets
 
 for repo_path, apps in secrets.repo_to_heroku_apps.iteritems():
     os.chdir(os.path.expanduser(repo_path))
-    subprocess.call('git pull upstream'.split())
+    subprocess.call('git pull'.split())
     proc = subprocess.Popen(
         'git show-ref --tags --dereference'.split(), stdout=subprocess.PIPE)
     stdout = proc.communicate()[0]
